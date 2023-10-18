046347f42e3acd65bf758bdafb54368a4b2a0572
Andy <andy374159@gmail.com>
2023-10-18 09:45:59 -0500

    revert mobile

--- src/components/attempt.jsx
+++ src/components/attempt.jsx
@@ -1,7 +1,7 @@
 export default function Attempts({ attemptDetails }) {
     return attemptDetails.map((attemptDetail) => {
         return (
-            <div style={{ borderColor: attemptDetail.focus ? '#ffffff' : '#2d4358' }} class="w-3/6 sm:w-full border-2 border-[#afcbdd]">
+            <div style={{ borderColor: attemptDetail.focus ? '#ffffff' : '#2d4358' }} class="w-3/6 border-2 border-[#afcbdd]">
                 <p class="inline-block text-white">
                     {attemptDetail.value === 'Skipped' ? '❌ Skipped' : attemptDetail.value}</p>
             </div>
